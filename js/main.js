a92c7ccd7851672a09bca6b66c1d999f28e96467
amalker <it-amalker@yandex.ru>
2019-10-27 21:52:32 +0300

    add script to move promo slider via circle buttons

--- js/main.js
+++ js/main.js
@@ -6,7 +6,12 @@ let buyPopup = document.querySelector(".modal-cart-added");
 let buyButtons = document.querySelectorAll(".buy-button");
 let buyCloseButton = document.querySelector(".modal-cart-added-close-button");
 
+let circleSwitches = document.querySelectorAll(".slider-switch");
+let promoSliders = document.querySelectorAll(".slider");
+
+
 //map popup
+
 if (mapPopup) {
     mapButton.addEventListener("click", function () {
         mapPopup.classList.add("modal-map--show");
@@ -17,21 +22,39 @@ if (mapPopup) {
     })
 }
 
-
 //buy popup
 
-var openBuyPopup = function (elem) {
+let openBuyPopup = function (elem) {
     elem.addEventListener("click", function () {
         buyPopup.classList.add("modal-cart-added--show");
     });
 }
 
 if (buyPopup) {
-    for (var i = 0; i < buyButtons.length; i++) {
+    for (let i = 0; i < buyButtons.length; i++) {
         openBuyPopup(buyButtons[i]);
     }
 
     buyCloseButton.addEventListener("click", function () {
         buyPopup.classList.remove("modal-cart-added--show");
     })
+}
+
+// promo slider
+
+let togglePromoSlider = function (elem) {
+    elem.addEventListener("click", function () {
+        for (let i = 0; i < circleSwitches.length; i++) {
+            circleSwitches[i].classList.remove("active");
+            promoSliders[i].classList.remove("active");
+        }
+        let circleSwitchesArray = Array.prototype.slice.call(circleSwitches);
+        let sliderIndex = circleSwitchesArray.indexOf(elem);
+        elem.classList.add("active");
+        promoSliders[sliderIndex].classList.add("active");
+    });
+}
+
+for (let i = 0; i < circleSwitches.length; i++) {
+    togglePromoSlider(circleSwitches[i]);
 }
