c4c8f70fcfe9b0317dfeadd72418f724b2370178
amalker <it-amalker@yandex.ru>
2019-10-28 09:52:55 +0300

    add services slider script

--- js/main.js
+++ js/main.js
@@ -10,6 +10,8 @@ let circleSwitches = document.querySelectorAll(".slider-switch");
 let arrowSwitches = document.querySelectorAll(".slider-arrow");
 let promoSliders = document.querySelectorAll(".slider");
 
+let servicesSwitches = document.querySelectorAll(".services-item");
+let servicesSliders = document.querySelectorAll(".services-slider");
 
 //map popup
 
@@ -72,4 +74,24 @@ let switchPromoSlider = function (elem) {
 for (let i = 0; i < circleSwitches.length; i++) {
     togglePromoSlider(circleSwitches[i]);
     switchPromoSlider(arrowSwitches[i]);
+}
+
+//services slider
+
+let toggleServicesSlider = function (elem) {
+    elem.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        for (let i = 0; i < servicesSwitches.length; i++) {
+            servicesSwitches[i].classList.remove("active");
+            servicesSliders[i].classList.remove("active");
+        }
+        let servicesSwitchesArray = Array.prototype.slice.call(servicesSwitches);
+        let sliderIndex = servicesSwitchesArray.indexOf(elem);
+        elem.classList.add("active");
+        servicesSliders[sliderIndex].classList.add("active");
+    });
+}
+
+for (let i = 0; i < servicesSwitches.length; i++) {
+    toggleServicesSlider(servicesSwitches[i]);
 }
